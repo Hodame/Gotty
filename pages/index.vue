9f276b7a09f39d9f18bb5c2ad118ddc784a579d5
hodame <hodame04@gmail.com>
2023-06-01 00:36:03 +0500

    add popular, latest, reviews section

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ const { data: gamePopular } = await useFetch<PageableList<GameCardInfo>>('/api/p
 
 <template>
 	<div class="main my-5">
-		<section class="main__popular">
+		<section class="main__popular mb-8">
 			<div class="main__title text-5xl font-semibold mb-5">Popular</div>
 
 			<Swiper :autoplay="{
@@ -14,7 +14,32 @@ const { data: gamePopular } = await useFetch<PageableList<GameCardInfo>>('/api/p
 				disableOnInteraction: true
 			}" :slides-per-view="4" loop :space-between="20" class="main__popular-cards">
 				<SwiperSlide v-for="(game, idx) in gamePopular?.results" :key="idx">
-					<GameCardsDefault :game-info="game" />
+					<CardsGameDefault :game-info="game" />
+				</SwiperSlide>
+			</Swiper>
+		</section>
+		
+		<section class="main__releases mb-8">
+			<div class="main__title text-5xl font-semibold mb-5">Latest release</div>
+
+			<Swiper :autoplay="{
+				delay: 8000,
+				disableOnInteraction: true
+			}" :slides-per-view="4" loop :space-between="20" class="main__popular-cards">
+				<SwiperSlide v-for="(game, idx) in gamePopular?.results" :key="idx">
+					<CardsGameDefault :game-info="game" />
+				</SwiperSlide>
+			</Swiper>
+		</section>
+		<section class="main__reviews mb-8">
+			<div class="main__title text-5xl font-semibold">Recent reviews</div>
+
+			<Swiper :autoplay="{
+				delay: 8000,
+				disableOnInteraction: true
+			}" :slides-per-view="4" loop :space-between="20" class="main__popular-cards pt-9">
+				<SwiperSlide v-for="(game, idx) in gamePopular?.results" :key="idx" class="pt-10">
+					<CardsReviewMain  />
 				</SwiperSlide>
 			</Swiper>
 		</section>
