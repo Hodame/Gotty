d90ecd7e696b6a7a29434971533977db0bb99ff0
hodame <hodame04@gmail.com>
2023-06-04 16:52:09 +0500

    create slider for review

---
+++ components/customSlider/review.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { GameCardInfo, PageableList } from '~/global';
+
+defineProps<{
+  slides?: PageableList<GameCardInfo> | null
+}>()
+</script>
+
+<template>
+  <Swiper :autoplay="{
+    delay: 8000,
+    disableOnInteraction: true
+  }" :slides-per-view="4" :space-between="20" class="main__popular-cards pt-9">
+    <SwiperSlide v-for="(review, idx) in 4" :key="idx" class="pt-10">
+      <CardsReviewMain />
+    </SwiperSlide>
+  </Swiper>
+</template>
