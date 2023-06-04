9fd3d34f62d03f7de2baf24b417c55f437d1ae87
hodame <hodame04@gmail.com>
2023-06-04 16:52:47 +0500

    create profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+const user = useUser()
+</script>
+
 <template>
-	<div>profile</div>
+	<div>
+		<div class="flex justify-between">
+			<div>
+				<UAvatar :src="user.user_metadata.avatar" :alt="user.user_metadata.nickname"/>
+				<p>{{ user.user_metadata.nickname }}</p>
+			</div>
+
+			<div class="grid grid-cols-4 gap-6">
+				<div>
+					<h1>Games</h1>
+					<p>{{ user.user_metadata.games }}</p>
+				</div>
+
+				<div>
+					<h1>Playing</h1>
+					<p>{{ user.user_metadata.playing }}</p>
+				</div>
+
+				<div>
+					<h1>Want</h1>
+					<p>{{ user.user_metadata.want }}</p>
+				</div>
+
+				<div>
+					<h1>Beaten</h1>
+					<p>{{ user.user_metadata.beaten }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
