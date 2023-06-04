55e3219ba0b300ed5c26ccbecb84df2400e466b7
hodame <hodame04@gmail.com>
2023-06-04 16:52:00 +0500

    create custom slide default

---
+++ components/customSlider/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { GameCardInfo, PageableList } from '~/global';
+
+defineProps<{
+  slides: PageableList<GameCardInfo> | null
+}>()
+</script>
+
+<template>
+  <Swiper :autoplay="{
+    delay: 4000,
+    disableOnInteraction: true
+  }" :slides-per-view="4" :space-between="20" :effect="'coverflow'" style="overflow: visible;">
+    <SwiperSlide v-for="(game, idx) in slides?.results" :key="idx">
+      <NuxtLink :to="{ path: '/game/' + game.id }">
+        <CardsGameDefault :game-info="game" />
+      </NuxtLink>
+    </SwiperSlide>
+  </Swiper>
+</template>
