697cca2434a1b5240e1b588d8b2738aa2b1e3849
hodame <hodame04@gmail.com>
2023-06-04 16:52:37 +0500

    fix default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,14 @@
 <template>
-	<div class="default max-w-[1200px] my-0 mx-auto">
-		<NavBar/>
-
-		<div>
-			<slot/>
+	<div class="bg-[url('~/assets/img/noise.webp')] w-full h-full fixed -z-10">
+	</div>
+	<NuxtLoadingIndicator />
+	<div
+		class="border-b border-color fixed w-full z-50 bg-white/75 backdrop-blur-none backdrop-blur-xl dark:bg-cool-900/75 ">
+		<NavBar class="max-w-[1200px] mx-auto px-4" />
+	</div>
+	<div class="default max-w-[1200px] mx-auto px-4">
+		<div class="pt-24">
+			<slot />
 		</div>
 	</div>
 </template>
